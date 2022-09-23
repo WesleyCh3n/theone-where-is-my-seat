4d7b419c2767c9c8f585f98fcf107bad6800ce82
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-09-23 14:39:15 +0800

    💄 update title arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,21 @@ function App() {
       className="h-screen flex flex-col justify-center items-center p-2"
       style={{ backgroundColor: "#faf0e6" }}
     >
-      <div className="mb-4">
-        <span className="text-2xl mr-2" style={{ color: "#A57A5A" }}>the One</span>
-        <span className="text-lg" style={{ color: "#C8AE7E" }}>- Save The Day -</span>
+      <div className="mb-8">
+        <span className="text-2xl mr-2" style={{ color: "#A57A5A" }}>
+          the One
+        </span>
+        <span className="text-lg" style={{ color: "#C8AE7E" }}>
+          - Save The Day -
+        </span>
       </div>
-      <span className="text-5xl text-gray-600 mb-4">承恩家妤婚禮</span>
+      <span className="text-[34px] text-gray-600 mb-8">承恩家妤婚禮</span>
       <span className="text-3xl text-gray-600 mb-12">座位查詢系統</span>
       <input
         className="shadow border rounded-xl min-w-[300px] w-1/3 py-3 px-3
         text-2xl text-gray-700 text-center"
         type="text"
-        placeholder="請輸入您的名字"
+        placeholder="請輸入您的全名"
         onChange={(e) => {
           setName(e.target.value);
           console.log(
@@ -31,9 +35,25 @@ function App() {
       />
       <span className="text-3xl text-gray-600 m-12">
         您的位子 : {name !== ""
-          ? name in seat ? seat[name as keyof typeof seat] : "找不到你的名字QQ"
+          ? name in seat
+            ? seat[name as keyof typeof seat]
+            : (
+              <span className="text-2xl">
+                {"\n"}找不到您的名字{String.fromCodePoint(parseInt("1F616", 16))}
+              </span>
+            )
           : "..."}
       </span>
+      <a
+        className="fixed text-sm text-gray-300 bottom-1 right-2"
+        style={{
+          color: "#D2BD96"
+        }}
+        href="https://github.com/WesleyCh3n"
+        target="_blank"
+      >
+        @WesleyCh3n
+      </a>
     </div>
   );
 }
